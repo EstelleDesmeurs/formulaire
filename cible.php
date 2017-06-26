0982b5792a6811aa6064f4f994440f13d7745556
EstelleDesmeurs <estelle.desmeurs@gmail.com>
2017-06-26 16:47:17 +0200

    incluant confirmation par e-mail (webhost)

--- cible.php
+++ cible.php
@@ -6,28 +6,23 @@ function sanitize($x) {
     return $x;
 }
  
-$firstName = sanitize($_POST['firstName']);
-$lastName = sanitize($_POST['lastName']);                             
+$name = sanitize($_POST['name']);                             
 $message = sanitize($_POST['message']);
 
 $email = $_POST['email'];
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
                    
-function validate($y) {
-    $y= filter_var($y, FILTER_VALIDATE_STRING);
+/*function validate($y) {
+    $y = filter_var($y, FILTER_VALIDATE_STRING);
     return $y;
-}
+}*/
                                          
-$firstName = validate($_POST['firstName']);
-$lastName = validate($_POST['lastName']);                            
-$message = validate($_POST['message']);
 
 
 if(isset($_POST['pouletteform'])) {
     if(
-        !empty ($_POST['firstName']) AND 
-        !empty ($_POST['lastName']) AND 
+        !empty ($_POST['name']) AND 
         !empty ($_POST['email']) AND 
         !empty ($_POST['message'])
     ) {
@@ -40,13 +35,25 @@ if(isset($_POST['pouletteform'])) {
 }   
 
                                          
-if (isset($email)) {
+/*if (isset($email)) {
     echo("Email is valid");
 } 
 else {
     echo("Email is not valid");
-}                                      
+}       */                               
 
+if (!$email){
+echo "Invalid Sender's Email";
+}
+else{
+$subject = 'Test';
+$indexMessage = $message;
+$headers = 'From:'. $email . "\r\n"; // Sender's Email
+$headers .= "\r\n"; // Carbon copy to Sender
+// Send Mail By PHP Mail Function
+mail("estelle.desmeurs@gmail.com", $subject, $indexMessage, $headers);
+echo "Your mail has been sent successfuly ! Thank you for your feedback";
+}
 
 
 
